5ea466d10648beb881cde33a53779874cb899fce
tomle <tomle@DESKTOP-CB22RD9>
2019-09-27 14:03:41 +0800

    PUBLISH FIRST BASIC APP DATA

---
+++ ABCTuitionCentre/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/abc.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table>
+        <tr>
+            <td>
+                <%--<telerik:RadScriptManager ID="RadScriptManager1" runat="server"></telerik:RadScriptManager>--%>
+            </td>
+        </tr>
+        <tr>
+        <td>
+    <telerik:RadLabel ID="RadLabel1" runat="server">
+        Test Labels
+    </telerik:RadLabel>
+    <telerik:RadTextBox ID="RadTextBox1" Runat="server" EmptyMessage="Search..." LabelWidth="64px" Resize="None" Width="160px">
+    </telerik:RadTextBox>
+            </td>
+    </tr>
+   </table>
+</asp:Content>
+
---
+++ ABCTuitionCentre/blank.master.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class blank : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            if (Request.Params["MsgContent"] != null)
+            {
+                string MsgContent = Convert.ToString(Request.Params["MsgContent"]).Trim();
+                MainRadNotification.Text = MsgContent;
+                MainRadNotification.Title = "ABC Tuition Centre";
+                if (Request.Params["MsgTitle"] != null)
+                {
+                    MainRadNotification.Title = Convert.ToString(Request.Params["MsgTitle"]).Trim();
+                }
+                MainRadNotification.TitleIcon = "info";
+                MainRadNotification.ContentIcon = "info";
+                if (Request.Params["MsgType"] != null)
+                {
+                    string MsgType = Convert.ToString(Request.Params["MsgType"]).Trim();
+                    MainRadNotification.TitleIcon = MsgType;
+                    MainRadNotification.ContentIcon = MsgType;
+                }
+
+                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.Center;
+                if (Request.Params["MsgPos"] != null)
+                {
+                    switch (Convert.ToString(Request.Params["MsgPos"]).Trim().ToUpper())
+                    {
+                        case "1":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.TopLeft;
+                                break;
+                            }
+                        case "2":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.TopCenter;
+                                break;
+                            }
+                        case "3":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.TopRight;
+                                break;
+                            }
+                        case "4":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.MiddleLeft;
+                                break;
+                            }
+                        case "5":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.Center;
+                                break;
+                            }
+                        case "6":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.MiddleRight;
+                                break;
+                            }
+                        case "7":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.BottomLeft;
+                                break;
+                            }
+                        case "8":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.BottomCenter;
+                                break;
+                            }
+                        case "9":
+                            {
+                                MainRadNotification.Position = Telerik.Web.UI.NotificationPosition.BottomRight;
+                                break;
+                            }
+                    }
+
+                }
+                MainRadNotification.Animation = Telerik.Web.UI.NotificationAnimation.Fade;
+                MainRadNotification.Show();
+            }
+        }
+    }
+}
